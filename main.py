42cedd5a81c3c9ce62c5a448a4e8bea06b4b1753
davidAlgis <david.algis@tutamail.com>
2024-07-03 19:39:48 +0200

    Add psd merger and new requirement

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PIL import Image
 from tqdm import tqdm
 import PyPDF2  # Make sure this line is included
 import io
+from psd_tools import PSDImage  # Import the necessary module for PSD files
 
 
 class PDF(FPDF):
@@ -23,14 +24,19 @@ class PDF(FPDF):
         self.image(image_path, x=0, y=0, w=new_width, h=new_height)
 
 
+def convert_psd_to_png(input_folder='.'):
+    png_files = []
+    for psd_file in tqdm([f for f in os.listdir(input_folder) if f.endswith('.psd')], desc="Converting PSD to PNG"):
+        psd_image = PSDImage.open(os.path.join(input_folder, psd_file))
+        png_file = os.path.join(input_folder, psd_file.replace('.psd', '.png'))
+        psd_image.composite().save(png_file)
+        png_files.append(png_file)
+    return png_files
+
+
 def png_to_pdf(input_folder='.', pdf_filename='output.pdf', verso_image=None):
     pdf = PDF()
-    if verso_image == None:
-        png_files = [f for f in os.listdir(input_folder) if f.endswith(
-            '.png')]
-    else:
-        png_files = [f for f in os.listdir(input_folder) if f.endswith(
-            '.png') and f != os.path.basename(verso_image)]
+    png_files = [f for f in os.listdir(input_folder) if f.endswith('.png')]
 
     if not png_files:
         print("No PNG files found in the specified directory.")
@@ -46,24 +52,27 @@ def png_to_pdf(input_folder='.', pdf_filename='output.pdf', verso_image=None):
 
 
 def merge_pdfs_and_images(input_folder='.', output_filename='merged.pdf', verso_image=None):
-    pdf_files = [f for f in os.listdir(input_folder) if f.endswith('.pdf')]
-    if verso_image:
-        png_files = [f for f in os.listdir(input_folder) if f.endswith(
-            '.png') and f != os.path.basename(verso_image)]
-    else:
-        png_files = [f for f in os.listdir(input_folder) if f.endswith('.png')]
-    if not pdf_files and not png_files:
-        print("No PDF or PNG files found in the specified directory.")
+    # Convert PSD files to PNG files first
+    psd_converted_files = convert_psd_to_png(input_folder)
+
+    # Add existing PNG files
+    all_png_files = [os.path.join(input_folder, f) for f in os.listdir(
+        input_folder) if f.endswith('.png')]
+    png_files = list(set(all_png_files) - set(psd_converted_files))
+
+    pdf_files = [os.path.join(input_folder, f)
+                 for f in os.listdir(input_folder) if f.endswith('.pdf')]
+
+    if not pdf_files and not png_files and not psd_converted_files:
+        print("No PDF or image files found in the specified directory.")
         return
 
     merger = PyPDF2.PdfMerger()
 
-    if png_files:
+    if psd_converted_files or png_files:
         pdf = PDF()
-        for png_file in tqdm(png_files, desc="Processing images"):
-            print(len(png_files))
-            pdf.add_page_with_image(os.path.join(input_folder, png_file))
-            print("add")
+        for png_file in tqdm(psd_converted_files + png_files, desc="Processing images"):
+            pdf.add_page_with_image(png_file)
             if verso_image:
                 pdf.add_page_with_image(verso_image)
 
@@ -74,9 +83,12 @@ def merge_pdfs_and_images(input_folder='.', output_filename='merged.pdf', verso_
 
     if pdf_files:
         for pdf_file in tqdm(pdf_files, desc="Merging PDFs"):
-            merger.append(os.path.join(input_folder, pdf_file))
+            merger.append(pdf_file)
 
-    merger.write(output_filename)
+    with open(output_filename, 'wb') as fout:
+        merger.write(fout)
+
+    merger.close()
     print(f"PDFs and images merged successfully: {output_filename}")
 
 
@@ -98,13 +110,13 @@ def test_images_to_pdf():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Generate a PDF from PNG images and merge with existing PDFs.')
+        description='Generate a PDF from PNG and PSD images and merge with existing PDFs.')
     parser.add_argument('-i', '--input', type=str, default='.',
                         help='Path of the input folder containing pictures or PDFs (default: current folder)')
     parser.add_argument('-o', '--output', type=str, default='output.pdf',
                         help='Path of the output generated PDF (default: ./output.pdf)')
     parser.add_argument('-v', '--verso', type=str,
-                        help='Path of an image to add after each PNG (verso)')
+                        help='Path of an image to add after each PNG or PSD (verso)')
     parser.add_argument('-t', '--test', action='store_true',
                         help='Run a test to create a PDF from images in the test folder')
 
