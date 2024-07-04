1c67c76d3e16f28b598ef4625a73aaf296fdde63
davidAlgis <david.algis@tutamail.com>
2024-07-04 08:07:25 +0200

    Add an argument to split the images

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import argparse
 from fpdf import FPDF
 from PIL import Image
 from tqdm import tqdm
-import PyPDF2  # Make sure this line is included
+import PyPDF2
 import io
-from psd_tools import PSDImage  # Import the necessary module for PSD files
+from psd_tools import PSDImage
 
 
 class PDF(FPDF):
@@ -34,7 +34,19 @@ def convert_psd_to_png(input_folder='.'):
     return png_files
 
 
-def png_to_pdf(input_folder='.', pdf_filename='output.pdf', verso_image=None):
+def split_image(image_path):
+    img = Image.open(image_path)
+    img_width, img_height = img.size
+    left_img = img.crop((0, 0, img_width // 2, img_height))
+    right_img = img.crop((img_width // 2, 0, img_width, img_height))
+    left_img_path = image_path.replace('.png', '_left.png')
+    right_img_path = image_path.replace('.png', '_right.png')
+    left_img.save(left_img_path)
+    right_img.save(right_img_path)
+    return left_img_path, right_img_path
+
+
+def png_to_pdf(input_folder='.', pdf_filename='output.pdf', verso_image=None, split=False):
     pdf = PDF()
     png_files = [f for f in os.listdir(input_folder) if f.endswith('.png')]
 
@@ -43,7 +55,16 @@ def png_to_pdf(input_folder='.', pdf_filename='output.pdf', verso_image=None):
         return
 
     for png_file in tqdm(png_files, desc="Processing images"):
-        pdf.add_page_with_image(os.path.join(input_folder, png_file))
+        if split:
+            left_img_path, right_img_path = split_image(
+                os.path.join(input_folder, png_file))
+            pdf.add_page_with_image(left_img_path)
+            pdf.add_page_with_image(right_img_path)
+            os.remove(left_img_path)
+            os.remove(right_img_path)
+        else:
+            pdf.add_page_with_image(os.path.join(input_folder, png_file))
+
         if verso_image:
             pdf.add_page_with_image(verso_image)
 
@@ -51,7 +72,7 @@ def png_to_pdf(input_folder='.', pdf_filename='output.pdf', verso_image=None):
     print(f"PDF created successfully: {pdf_filename}")
 
 
-def merge_pdfs_and_images(input_folder='.', output_filename='merged.pdf', verso_image=None):
+def merge_pdfs_and_images(input_folder='.', output_filename='merged.pdf', verso_image=None, split=False):
     # Convert PSD files to PNG files first
     psd_converted_files = convert_psd_to_png(input_folder)
 
@@ -72,7 +93,15 @@ def merge_pdfs_and_images(input_folder='.', output_filename='merged.pdf', verso_
     if psd_converted_files or png_files:
         pdf = PDF()
         for png_file in tqdm(psd_converted_files + png_files, desc="Processing images"):
-            pdf.add_page_with_image(png_file)
+            if split:
+                left_img_path, right_img_path = split_image(png_file)
+                pdf.add_page_with_image(left_img_path)
+                pdf.add_page_with_image(right_img_path)
+                os.remove(left_img_path)
+                os.remove(right_img_path)
+            else:
+                pdf.add_page_with_image(png_file)
+
             if verso_image:
                 pdf.add_page_with_image(verso_image)
 
@@ -108,6 +137,20 @@ def test_images_to_pdf():
         print(f"ImagesToPDF test has failed! Error: {e}")
 
 
+def str2bool(v):
+    """
+    Converts a string argument to a boolean value.
+    """
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Generate a PDF from PNG and PSD images and merge with existing PDFs.')
@@ -117,6 +160,8 @@ if __name__ == '__main__':
                         help='Path of the output generated PDF (default: ./output.pdf)')
     parser.add_argument('-v', '--verso', type=str,
                         help='Path of an image to add after each PNG or PSD (verso)')
+    parser.add_argument('-s', '--split', type=str2bool, default=False,
+                        help='Split each page in the PDF into two along the width (default is False)')
     parser.add_argument('-t', '--test', action='store_true',
                         help='Run a test to create a PDF from images in the test folder')
 
@@ -125,4 +170,4 @@ if __name__ == '__main__':
     if args.test:
         test_images_to_pdf()
     else:
-        merge_pdfs_and_images(args.input, args.output, args.verso)
+        merge_pdfs_and_images(args.input, args.output, args.verso, args.split)
