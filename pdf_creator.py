8fdd5f611ad8160fa24fb2ef8bf1f0b1184fb156
davidAlgis <david.algis@tutamail.com>
2024-10-31 16:07:44 +0100

    Add more input file types

--- pdf_creator.py
+++ pdf_creator.py
@@ -1,6 +1,6 @@
 import os
 from psd_to_png import convert_psd_to_png
-from picture_to_pdf import picture_to_pdf
+from picture_to_pdf import picture_to_pdf  # Updated import to match the renamed file
 from pdf_merger import merge_pdfs
 from tqdm import tqdm
 import shutil
@@ -23,8 +23,8 @@ def create_pdf(input_path, output_pdf, verso_image=None, split=False):
                     "temp_pdf",
                     f"{os.path.splitext(os.path.basename(png_file))[0]}.pdf"),
                 verso_image, split)
-        # If it's a PNG file, convert directly to PDF
-        elif file_ext == '.png':
+        # If it's an image file (PNG, JPG, JPEG), convert directly to PDF
+        elif file_ext in ['.png', '.jpg', '.jpeg']:
             picture_to_pdf(
                 input_path,
                 os.path.join(
@@ -37,7 +37,7 @@ def create_pdf(input_path, output_pdf, verso_image=None, split=False):
                         os.path.join("temp_pdf", os.path.basename(input_path)))
         else:
             print(
-                "Unsupported file format. Please provide a PNG, PSD, or PDF file."
+                "Unsupported file format. Please provide a PNG, JPG, JPEG, PSD, or PDF file."
             )
             return
     else:
@@ -47,12 +47,15 @@ def create_pdf(input_path, output_pdf, verso_image=None, split=False):
                 desc="Converting PSD to PNG"):
             convert_psd_to_png(os.path.join(input_path, psd_file))
 
-        png_files = [f for f in os.listdir(input_path) if f.endswith('.png')]
-        for png_file in tqdm(png_files, desc="Converting PNG to PDF"):
+        image_files = [
+            f for f in os.listdir(input_path)
+            if f.endswith(('.png', '.jpg', '.jpeg'))
+        ]
+        for image_file in tqdm(image_files, desc="Converting images to PDF"):
             picture_to_pdf(
-                os.path.join(input_path, png_file),
+                os.path.join(input_path, image_file),
                 os.path.join("temp_pdf",
-                             f"{os.path.splitext(png_file)[0]}.pdf"),
+                             f"{os.path.splitext(image_file)[0]}.pdf"),
                 verso_image, split)
 
         pdf_files = [f for f in os.listdir(input_path) if f.endswith('.pdf')]
